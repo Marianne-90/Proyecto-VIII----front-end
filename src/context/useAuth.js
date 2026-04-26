87153ef93fa655b2ca78f7b74eb71d284ab4bfe6
Marianne <marianne.garridom@gmail.com>
2026-04-26 22:31:37 +0200

    opt

--- src/context/useAuth.js
+++
@@ -1,8 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "./auth-context.js";
-
-export function useAuth() {
-  const ctx = useContext(AuthContext);
-  if (!ctx) throw new Error("useAuth debe usarse dentro de un <AuthProvider>");
-  return ctx;
-}
